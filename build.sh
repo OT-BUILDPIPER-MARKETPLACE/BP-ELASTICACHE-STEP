1720ac00c188329b4b85ce5d72028f87cad5d823
Sandeep <sandeep@opstree.com>
2022-11-15 14:12:05 +0530

    Added code to save status of task

--- build.sh
+++ build.sh
@@ -17,18 +17,24 @@ case "$INSTRUCTION" in
 
   plan)
     terraform plan -var-file="terraform.tfvars"
+    TASK_STATUS=$?
     ;;
 
   apply)
     terraform apply -auto-approve -var-file="terraform.tfvars"
+    TASK_STATUS=$?
     ;;
 
   destroy)
     terraform destroy -auto-approve -var-file="terraform.tfvars"
+    TASK_STATUS=$?
     ;;
 
   *)
     logInfoMessage "Not a valid option"
+    TASK_STATUS=1
     ;;
 esac
+
+saveTaskStatus ${TASK_STATUS} ${ACTIVITY_SUB_TASK_CODE}
 
