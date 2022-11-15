af3a02e308b32d658bc353abf057b1f58c055d7f
Surabhi Maheshwari <surabhi95maheshwari@gmail.com>
2022-11-15 13:56:31 +0530

    updated variable

--- ELASTICACHE/variable.tf
+++ ELASTICACHE/variable.tf
@@ -4,6 +4,7 @@ variable "name" {
 }
 variable "notification_topic_arn" {
   type        = string
+  default     = null
   description = "An Amazon Resource Name (ARN) of an SNS topic to send ElastiCache notifications to. Example: arn:aws:sns:us-east-1:012345678999:my_sns_topic"
 }
 variable "security_group_ids" {
@@ -12,10 +13,12 @@ variable "security_group_ids" {
 }
 variable "snapshot_arns" {
   type        = list(string)
+  default     = null
   description = "A list of Amazon Resource Names (ARNs) that identify Redis RDB snapshot files stored in Amazon S3. The names object names cannot contain any commas."
 }
 variable "snapshot_name" {
   type        = string
+  default     = null
   description = "The name of a snapshot from which to restore data into the new node group. Changing the snapshot_name forces a new resource."
 }
 variable "subnet_ids" {
